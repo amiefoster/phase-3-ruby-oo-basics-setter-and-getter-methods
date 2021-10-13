93c8444b3f285762e6eb7fc92346ff6d3f527432
Amie Foster <amie.n.foster@gmail.com>
2021-10-12 22:48:51 -0600

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    #setter name
+    def name=(name)
+        @name = name
+    end
 
+    #getter name
+    def name
+        @name
+    end
+
+    #setter breed
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getter breed
+    def breed
+        @breed
+    end
 end
