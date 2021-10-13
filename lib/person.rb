0e3dd852f8b59aa6c0eb76fa883ab1f8c3b0bca3
Amie Foster <amie.n.foster@gmail.com>
2021-10-12 22:48:44 -0600

    commit tree 8e3f2beb1f76d32a92c6ed39f06ed9bfc1c3c848

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    #setter name
+    def name=(name)
+        @name = name
+    end
+
+    #getter name
+    def name
+        @name
+    end
+
+    #job setter
+    def job=(job)
+        @job = job
+    end
+
+    #job getter
+    def job
+        @job
+    end
+
 end
